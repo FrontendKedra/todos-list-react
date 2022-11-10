6111f7ebad65e63c1da96f7815a27f04e382391f
Szymon Kedra <s5zee@wp.pl>
2022-11-10 17:42:23 +0100

    added dispatching success and error actions

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,13 +1,29 @@
-import { takeEvery, call, put, select, takeLatest } from "redux-saga/effects";
+import {
+  takeEvery,
+  call,
+  put,
+  select,
+  takeLatest,
+  delay,
+} from "redux-saga/effects";
 import { getExampleTasks } from "./getExampleTasks";
 import { saveTasksInLocalStorage } from "./tasksLocalStorage";
-import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+import {
+  exampleTasksError,
+  exampleTasksSuccess,
+  fetchExampleTasks,
+  selectTasks,
+  setTasks,
+} from "./tasksSlice";
 
 function* fetchExampleTasksHandler() {
   try {
+    yield delay(2000);
+    yield put(exampleTasksSuccess());
     const exampleTasks = yield call(getExampleTasks);
     yield put(setTasks(exampleTasks));
   } catch (error) {
+    yield put(exampleTasksError());
     yield call(alert, "cos poszlo nie tak");
   }
 }
