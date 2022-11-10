f690fa841273f3d63e63cc2a565034cb9136f093
Szymon Kedra <s5zee@wp.pl>
2022-11-10 17:44:37 +0100

    atr. disabled and control over text in button

--- src/features/tasks/ExampleTasksButton/index.js
+++ src/features/tasks/ExampleTasksButton/index.js
@@ -1,13 +1,24 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "../Button/styled";
-
-import { fetchExampleTasks } from "../tasksSlice";
+import {
+  fetchExampleTasks,
+  selectExampleTasks,
+  exampleTasksLoading,
+} from "../tasksSlice";
 
 const ExampleTasksButton = () => {
   const dispatch = useDispatch();
+  const buttonState = useSelector(selectExampleTasks);
   return (
-    <Button onClick={() => dispatch(fetchExampleTasks())}>
-      Pobierz przykładowe zadania
+    <Button
+      disabled={buttonState}
+      onClick={() => {
+        dispatch(fetchExampleTasks());
+        dispatch(exampleTasksLoading());
+      }}
+    >
+    {buttonState ? "Trwa pobieranie zadań" : "Pobierz przykładowe zadania"} 
+      
     </Button>
   );
 };
