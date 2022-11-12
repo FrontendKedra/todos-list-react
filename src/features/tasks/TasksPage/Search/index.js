7400eb634e52ef52a23e62366887f08ad647f4d4
Szymon Kedra <s5zee@wp.pl>
2022-11-12 13:14:11 +0100

    implemented custom hooks in Search component

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,32 +1,30 @@
-import { useHistory, useLocation } from "react-router-dom";
 import Input from "../../Input";
+import {
+  useQueryParameter,
+  useReplaceQueryParameter,
+} from "../../queryParameters";
 import searchQueryParamName from "../searchQueryParamName";
 import { Wrapper } from "./style";
 
 const Search = () => {
-  const location = useLocation();
-  const history = useHistory();
-  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+  const query = useQueryParameter(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
 
   const onInputChange = ({ target }) => {
-    const searchParams = new URLSearchParams(location.search);
-    if (target.value.trim() === "") {
-      searchParams.delete(searchQueryParamName);
-    } else {
-      searchParams.set(searchQueryParamName, target.value);
-    }
-
-    history.push(`${location.pathname}?${searchParams.toString()}`);
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: target.value.trim() !== "" ? target.value : undefined,
+    });
   };
 
   return (
     <Wrapper>
-    <Input
-      placeholder="filtruj zadania"
-      value={query || ""}
-      onChange={onInputChange}
+      <Input
+        placeholder="filtruj zadania"
+        value={query || ""}
+        onChange={onInputChange}
       />
-      </Wrapper>
+    </Wrapper>
   );
 };
 export default Search;
