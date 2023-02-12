89e1cb7cd55e66d71d1d10fd9890c930a81d2020
Szymon Kedra <s5zee@wp.pl>
2023-02-12 15:28:58 +0100

    tasksLocalStorage typed

---
+++ src/features/tasks/tasksLocalStorage.ts
@@ -0,0 +1,12 @@
+import { Tasks } from "../../types/interfaces";
+
+const localStorageKey = "tasks";
+
+export const saveTasksInLocalStorage = (tasks: Tasks[]) => {
+  localStorage.setItem(localStorageKey, JSON.stringify(tasks));
+};
+
+export const getTasksFromLocalStorage = (): Tasks[] => {
+  const tasksFromLocalStorage = localStorage.getItem("tasks");
+  return tasksFromLocalStorage ? JSON.parse(tasksFromLocalStorage) : [];
+};
