23f70beee2259d17ee88bcb699db8c6f831eecd9
Szymon Kedra <s5zee@wp.pl>
2023-02-12 15:28:38 +0100

    getExampleTasks function typed

--- src/features/tasks/getExampleTasks.ts
+++ src/features/tasks/getExampleTasks.ts
@@ -1,8 +1,10 @@
+import { Tasks } from "../../types/interfaces";
+
 export const getExampleTasks = async () => {
   const response = await fetch("todos-list-react/exampleTasks.json");
   if (!response.ok) {
     new Error(response.statusText);
   }
 
-  return await response.json();
+  return (await response.json()) as Promise<Tasks[]>;
 };
