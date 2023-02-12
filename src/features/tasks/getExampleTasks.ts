58c3962c48938ce44ba8ded5017fdbc7c78d2e2b
Szymon Kedra <s5zee@wp.pl>
2023-02-12 22:02:38 +0100

    .

--- src/features/tasks/getExampleTasks.ts
+++ src/features/tasks/getExampleTasks.ts
@@ -1,8 +1,8 @@
 import { Tasks } from "../../types/interfaces";
 
 export const getExampleTasks = async () => {
-  const response = await fetch("https://frontendkedra.github.io/todos-list-react/exampleTasks.json");
-  if (!response.ok) {
+  const response = await fetch("/todos-list-react/exampleTasks.json");
+  if (!response.ok) { 
     new Error(response.statusText);
   }
 
