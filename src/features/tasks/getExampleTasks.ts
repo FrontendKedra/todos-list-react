f56c624c8a14fcddf2e4ed5c7cf3756504db5951
Szymon Kedra <s5zee@wp.pl>
2023-03-12 20:39:55 +0100

    throw word added

--- src/features/tasks/getExampleTasks.ts
+++ src/features/tasks/getExampleTasks.ts
@@ -3,7 +3,7 @@ import { Tasks } from "../../interfaces/interfaces";
 export const getExampleTasks = async () => {
   const response = await fetch("/todos-list-react/exampleTasks.json");
   if (!response.ok) {
-    new Error(response.statusText);
+    throw new Error(response.statusText);
   }
 
   return (await response.json()) as Promise<Tasks[]>;
